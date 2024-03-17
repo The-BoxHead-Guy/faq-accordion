2286d5aef2df2eefb4eb5178d2b42b11c679dfad
Jhon Alessandro <astralgmd@gmail.com>
2024-03-16 21:36:31 -0400

    feat: make the accordeon FAQ display using 'click' and 'keyboard' navigation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,61 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Variables
   const accordeonItems = document.querySelectorAll(".accordeon");
+  console.log(accordeonItems);
+
+  // Keyboard navigation
+  const handleNavigationFocus = (event) => {
+    // Creating an array of all possible selectable items
+    const focusableItems = [...accordeonItems];
+    const key = event.key;
+
+    // Getting the index of the currently focused item
+    const index = focusableItems.indexOf(document.activeElement);
+
+    // creates the index of the next element
+    let nextIndex = 0;
+
+    // Determining index change
+    if (key === "ArrowDown") {
+      event.preventDefault();
+      nextIndex = index + 1 < focusableItems.length ? index + 1 : 0;
+      accordeonItems[nextIndex].focus();
+    } else if (key === "ArrowUp") {
+      event.preventDefault();
+      nextIndex = index > 0 ? index - 1 : 0;
+      accordeonItems[nextIndex].focus();
+    }
+  };
+
+  document.addEventListener("keydown", handleNavigationFocus);
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      triggerChange(event.target);
+    }
+
+    console.log(event.target);
+  });
 
   accordeonItems.forEach((item) => {
     const header = item.querySelector(".accordeon__header");
 
     header.addEventListener("click", () => {
-      const panel = item.querySelector(".accordeon__panel");
-      const accordeonIcon = item.querySelector(".accordeon__icon");
-      const title = item.getElementsByTagName("h3");
-
-      showAccordeon(panel);
-      changeIcon(accordeonIcon);
-      changeColor(title);
+      triggerChange(item);
     });
-
-    // todo: Navigate the questions and hide/show answers using keyboard navigation alone
   });
 
   // Functions
+  const triggerChange = (item) => {
+    const panel = item.querySelector(".accordeon__panel");
+    const icon = item.querySelector(".accordeon__icon");
+    const title = item.getElementsByTagName("h3");
+
+    showAccordeon(panel);
+    changeIcon(icon);
+    changeColor(title);
+  };
+
   function showAccordeon(arg) {
     arg.style.maxHeight = arg.style.maxHeight ? null : arg.scrollHeight + "px";
 
